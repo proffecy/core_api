f91eda1c7edda45fc698e88a529a12da0393dbb4
Sebram <admin@example.com>
2018-04-27 14:30:25 +0200

    fosuser config changed

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    App\CoreBundle\CoreBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
