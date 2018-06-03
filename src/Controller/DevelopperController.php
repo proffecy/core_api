f45467c9b3253e50eb5da79cad3bb7614c850ff5
Sebram <skonemailbox@gmail.com>
2018-06-03 22:10:05 +0200

    Dev Doc Controller

---
+++ src/Controller/DevelopperController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations\Route;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+class DevelopperController extends Controller
+{
+
+
+
+	/**
+     * @Get(
+     *     path = "/dev/doc/register",
+     *     name = "proffecy_doc"
+     * )
+     */
+    public function docAction()
+    {
+        $appname="Proffecy Core Api";
+        
+        return $this->render('/core/doc/register_documentation.html.twig', array(
+            'appname' => $appname,
+        ));
+    }
+
+
+    /**
+     * @Get(
+     *     path = "/dev/doc/auth",
+     *     name = "proffecy_doc_auth"
+     * )
+     */
+    public function docAuthAction()
+    {
+        $appname="Proffecy Core Api";
+        
+        return $this->render('/core/doc/auth_documentation.html.twig', array(
+            'appname' => $appname,
+        ));
+    }
+
+
+    /**
+     * @Get(
+     *     path = "/dev/doc/services",
+     *     name = "proffecy_doc_services"
+     * )
+     */
+    public function docServicesAction()
+    {
+        $appname="Proffecy Core Api";
+        
+        return $this->render('/core/doc/services_documentation.html.twig', array(
+            'appname' => $appname,
+        ));
+    }
+
+}
